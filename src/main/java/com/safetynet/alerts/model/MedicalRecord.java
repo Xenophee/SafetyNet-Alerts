fcd042c7fd31bee5f6e408dff496fb8f87737d34
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-06-21 12:58:58 +0200

    Correctif pour prendre en compte le LocalDate

--- src/main/java/com/safetynet/alerts/model/MedicalRecord.java
+++ src/main/java/com/safetynet/alerts/model/MedicalRecord.java
@@ -1,15 +1,17 @@
 package com.safetynet.alerts.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 
-// ! Corriger le système de validation des dates de naissance
+import java.time.LocalDate;
+
+
 public record MedicalRecord(
         @NotBlank(message = "Firstname is mandatory") String firstName,
         @NotBlank(message = "Lastname is mandatory") String lastName,
-        @NotNull(message = "Birthdate is mandatory") @PastOrPresent(message = "Birthdate can't be in the future") String birthdate,
+        @NotNull(message = "Birthdate is mandatory") @PastOrPresent(message = "Birthdate can't be in the future") @JsonFormat(pattern = "MM/dd/yyyy") LocalDate birthdate,
         String[] medications,
         String[] allergies
 ) {
