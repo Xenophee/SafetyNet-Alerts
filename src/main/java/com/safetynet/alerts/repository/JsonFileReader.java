33e45d5bc91f27e9b1f43283fa4072973d88332d
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-06-21 12:59:52 +0200

    Changement de dossier et correctif pour prendre en compte le LocalDate

--- src/main/java/com/safetynet/alerts/repository/JsonFileReader.java
+++ src/main/java/com/safetynet/alerts/repository/JsonFileReader.java
@@ -1,23 +1,28 @@
-package com.safetynet.alerts.config;
+package com.safetynet.alerts.repository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.safetynet.alerts.exception.JsonFileReadException;
 import com.safetynet.alerts.model.Data;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
 
+
+@Component
 public class JsonFileReader {
 
-    private static final String FILE_PATH = "resources/data.json";
+    private static final String FILE_PATH = "src/main/java/com/safetynet/alerts/repository/data.json";
     private final Data data;
 
     public JsonFileReader() throws JsonFileReadException {
         try {
             ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
             data = mapper.readValue(new File(FILE_PATH), Data.class);
         } catch (IOException e) {
-            throw new JsonFileReadException(FILE_PATH);
+            throw new JsonFileReadException();
         }
     }
 
