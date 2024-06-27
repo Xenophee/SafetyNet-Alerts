369b96df3ad947725d7bbf8e2370eee0984448ba
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-06-27 21:53:31 +0200

    Ajout de logger et de la javadoc. Changement de logique sur certaines méthodes pour les rendre plus permissive.

--- src/main/java/com/safetynet/alerts/service/FireStationService.java
+++ src/main/java/com/safetynet/alerts/service/FireStationService.java
@@ -7,11 +7,12 @@ import com.safetynet.alerts.model.Data;
 import com.safetynet.alerts.model.FireStation;
 import com.safetynet.alerts.model.MedicalRecord;
 import com.safetynet.alerts.model.Person;
-import com.safetynet.alerts.repository.JsonFileReader;
+import com.safetynet.alerts.repository.JsonFileHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.tinylog.Logger;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -20,70 +21,133 @@ import java.util.stream.IntStream;
 import static com.safetynet.alerts.util.BirthdateUtil.*;
 
 
+/**
+ * Service class for managing fire stations.
+ * This class provides methods for creating, updating, deleting fire stations, and retrieving information related to fire stations.
+ *
+ * @author Perrine Dassonville
+ * @version 1.0
+ *
+ * @see StationCoverageDTO
+ * @see FloodDTO
+ * @see FireDTO
+ * @see AlreadyExistException
+ * @see NotFoundException
+ * @see Data
+ * @see FireStation
+ * @see JsonFileHandler
+ */
 @Service
 public class FireStationService {
 
-    private final JsonFileReader jsonFileReader;
+    private final JsonFileHandler jsonFileHandler;
 
     @Autowired
-    public FireStationService(JsonFileReader jsonFileReader) {
-        this.jsonFileReader = jsonFileReader;
+    public FireStationService(JsonFileHandler jsonFileHandler) {
+        this.jsonFileHandler = jsonFileHandler;
     }
 
 
-
+    /**
+     * Creates a new fire station.
+     *
+     * @param fireStation The fire station to create.
+     * @return A ResponseEntity with the HTTP status.
+     * @throws AlreadyExistException if a fire station with the same address and station number already exists.
+     */
     public ResponseEntity<Void> create(FireStation fireStation) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Creating fire station : {}", fireStation);
+        Data data = jsonFileHandler.getData();
 
-        if (data.fireStations().contains(fireStation)) {
+        boolean fireStationExists = data.fireStations().stream()
+                .anyMatch(fs -> fs.address().equalsIgnoreCase(fireStation.address()) && fs.station() == fireStation.station());
+
+        if (fireStationExists) {
+            Logger.error("FireStation " + fireStation.station() + " with address " + fireStation.address() + " already exists");
             throw new AlreadyExistException("FireStation " + fireStation.station() + " with address " + fireStation.address() + " already exists");
         }
 
         data.fireStations().add(fireStation);
-        jsonFileReader.writeData(data);
+        jsonFileHandler.writeData(jsonFileHandler.sortFireStationsByStationNumber(data));
 
+        Logger.info("Fire station created : {}", fireStation);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 
+    /**
+     * Updates an existing fire station.
+     *
+     * @param fireStation The fire station to update.
+     * @return A ResponseEntity with the HTTP status.
+     * @throws NotFoundException if no fire station with the same address is found.
+     */
     public ResponseEntity<Void> update(FireStation fireStation) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Updating fire station : {}", fireStation);
+        Data data = jsonFileHandler.getData();
 
         List<FireStation> fireStations = data.fireStations();
         int index = IntStream.range(0, fireStations.size())
-                .filter(i -> fireStations.get(i).address().equals(fireStation.address()))
+                .filter(i -> fireStations.get(i).address().equalsIgnoreCase(fireStation.address()))
                 .findFirst()
-                .orElseThrow(() -> new NotFoundException("FireStation with address " + fireStation.address() + " not found"));
+                .orElseThrow(() -> {
+                    Logger.error("FireStation " + fireStation.station() + " with address " + fireStation.address() + " already exists");
+                    return new NotFoundException("FireStation with address " + fireStation.address() + " not found");
+                });
 
         fireStations.set(index, fireStation);
-        jsonFileReader.writeData(data);
+        jsonFileHandler.writeData(data);
 
+        Logger.info("Fire station updated : {}", fireStation);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
+    // ! A vérifier avec le souci sur le doublon d'adresse dans le json
+    /**
+     * Deletes a fire station.
+     *
+     * @param fireStation The fire station to delete.
+     * @return A ResponseEntity with the HTTP status.
+     * @throws NotFoundException if no fire station with the same address and station number is found.
+     */
     public ResponseEntity<Void> delete(FireStation fireStation) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Deleting fire station: {}", fireStation);
+        Data data = jsonFileHandler.getData();
 
-        if (!data.fireStations().contains(fireStation)) {
-            throw new NotFoundException("FireStation " + fireStation.station() + " with address " + fireStation.address() + " not found");
-        }
+        FireStation fireStationToDelete = data.fireStations().stream()
+                .filter(fs -> fs.address().equalsIgnoreCase(fireStation.address())
+                        && fs.station() == fireStation.station())
+                .findFirst()
+                .orElseThrow(() -> {
+                    Logger.error("FireStation " + fireStation.station() + " with address " + fireStation.address() + " not found");
+                    return new NotFoundException("FireStation " + fireStation.station() + " with address " + fireStation.address() + " not found");
+                });
 
-        data.fireStations().remove(fireStation);
-        jsonFileReader.writeData(data);
+        data.fireStations().remove(fireStationToDelete);
+        jsonFileHandler.writeData(data);
 
+        Logger.info("Fire station deleted: {}", fireStationToDelete);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
+    /**
+     * Retrieves persons station coverage by station number.
+     *
+     * @param stationNumber The station number to search for.
+     * @return A StationCoverageDTO containing the number of adults, children, and a list of persons covered by the station.
+     */
     public StationCoverageDTO getPersonsStationCoverage(int stationNumber) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Getting persons station coverage for station number: {}", stationNumber);
+        Data data = jsonFileHandler.getData();
 
         Set<String> stationAddresses = getAddressesByStation(stationNumber);
         Map<Person, MedicalRecord> personMedicalRecordMap = PersonService.mapPersonsToMedicalRecords(data);
 
         List<StationCoveragePersonInfoDTO> persons = data.persons().stream()
-                .filter(person -> stationAddresses.contains(person.address()))
+                .filter(person -> stationAddresses.stream()
+                        .anyMatch(stationAddress -> stationAddress.equalsIgnoreCase(person.address())))
                 .map(person -> new StationCoveragePersonInfoDTO(person.firstName(), person.lastName(), person.address(), person.phone()))
                 .toList();
 
@@ -99,20 +163,30 @@ public class FireStationService {
 
         int adults = persons.size() - children;
 
+        Logger.info("Successfully got persons station coverage for station number: {}", stationNumber);
         return new StationCoverageDTO(adults, children, persons);
     }
 
 
+    /**
+     * Retrieves homes by station numbers.
+     *
+     * @param stations The list of station numbers to search for.
+     * @return A list of FloodDTOs containing the station number and a map of addresses to a list of persons living at each address.
+     */
     public List<FloodDTO> getHomesByStations(List<Integer> stations) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Getting homes by stations: {}", stations);
+
+        Data data = jsonFileHandler.getData();
         Map<Person, MedicalRecord> personMedicalRecordMap = PersonService.mapPersonsToMedicalRecords(data);
 
-        return stations.stream()
+        List<FloodDTO> floodDTOList = stations.stream()
                 .map(stationNumber -> {
                     Set<String> stationAddresses = getAddressesByStation(stationNumber);
 
                     Map<String, List<FireFloodPersonInfoDTO>> personsByAddress = data.persons().stream()
-                            .filter(person -> stationAddresses.contains(person.address()))
+                            .filter(person -> stationAddresses.stream()
+                                    .anyMatch(stationAddress -> stationAddress.equalsIgnoreCase(person.address())))
                             .map(person -> {
                                 MedicalRecord medicalRecord = personMedicalRecordMap.get(person);
                                 return new AbstractMap.SimpleEntry<>(person.address(), new FireFloodPersonInfoDTO(person.firstName(), person.lastName(), person.phone(), getAge(medicalRecord.birthdate()), medicalRecord.medications(), medicalRecord.allergies()));
@@ -122,44 +196,84 @@ public class FireStationService {
                     return new FloodDTO(stationNumber, personsByAddress);
                 })
                 .toList();
+
+        Logger.info("Successfully got homes by stations: {}", stations);
+        return floodDTOList;
     }
 
 
+    /**
+     * Retrieves persons phones by station number.
+     *
+     * @param stationNumber The station number to search for.
+     * @return A set of phone numbers of persons covered by the station.
+     * @throws NotFoundException if no station with the given number is found.
+     */
     public Set<String> getPersonsPhonesByStation(int stationNumber) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Getting persons phones by station number: {}", stationNumber);
+        Data data = jsonFileHandler.getData();
 
         Set<String> stationAddresses = getAddressesByStation(stationNumber);
 
-        return data.persons().stream()
-                .filter(person -> stationAddresses.contains(person.address()))
+        Set<String> phones = data.persons().stream()
+                .filter(person -> stationAddresses.stream()
+                        .anyMatch(stationAddress -> stationAddress.equalsIgnoreCase(person.address())))
                 .map(Person::phone)
                 .collect(Collectors.toSet());
+
+        if (phones.isEmpty()) {
+            Logger.error("Station number " + stationNumber + " not found");
+            throw new NotFoundException("Station number " + stationNumber + " not found");
+        }
+
+        Logger.info("Successfully got persons phones by station number: {}", stationNumber);
+        return phones;
     }
 
 
+    /**
+     * Retrieves persons and station by address.
+     *
+     * @param address The address to search for.
+     * @return A FireDTO containing the station number and a list of persons living at the address.
+     * @throws NotFoundException if no station covering the given address is found.
+     */
     public FireDTO getPersonsAndStationByAddress(String address) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Getting persons and station by address: {}", address);
+        Data data = jsonFileHandler.getData();
         Map<Person, MedicalRecord> personMedicalRecordMap = PersonService.mapPersonsToMedicalRecords(data);
 
         FireStation fireStation = data.fireStations().stream()
-                .filter(station -> address.equals(station.address()))
+                .filter(station -> address.equalsIgnoreCase(station.address()))
                 .findFirst()
-                .orElseThrow(() -> new NotFoundException("No station found for address: " + address));
+                .orElseThrow(() -> {
+                    Logger.error("No station found for address: " + address);
+                    return new NotFoundException("No station found for address: " + address);
+                });
 
         List<FireFloodPersonInfoDTO> persons = data.persons().stream()
-                .filter(person -> address.equals(person.address()))
+                .filter(person -> address.equalsIgnoreCase(person.address()))
                 .map(person -> {
                     MedicalRecord medicalRecord = personMedicalRecordMap.get(person);
                     return new FireFloodPersonInfoDTO(person.firstName(), person.lastName(), person.phone(), getAge(medicalRecord.birthdate()), medicalRecord.medications(), medicalRecord.allergies());
                 })
                 .toList();
 
+        Logger.info("Successfully got persons and station by address: {}", address);
         return new FireDTO(fireStation.station(), persons);
     }
 
 
+    /**
+     * Retrieves addresses by station number.
+     *
+     * @param stationNumber The station number to search for.
+     * @return A set of addresses covered by the station.
+     * @throws NotFoundException if no station with the given number is found.
+     */
     public Set<String> getAddressesByStation(int stationNumber) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Getting addresses by station number: {}", stationNumber);
+        Data data = jsonFileHandler.getData();
 
         Set<String> stationAddresses = data.fireStations().stream()
                 .filter(fireStation -> stationNumber == fireStation.station())
@@ -167,9 +281,11 @@ public class FireStationService {
                 .collect(Collectors.toSet());
 
         if (stationAddresses.isEmpty()) {
+            Logger.error("Station number " + stationNumber + " not found");
             throw new NotFoundException("Station number " + stationNumber + " not found");
         }
 
+        Logger.info("Successfully got addresses by station number: {}", stationNumber);
         return stationAddresses;
     }
 }
