2a77eac02c005bc10e8cbc9f41003c6f28634283
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-06-27 21:51:40 +0200

    Ajout de logger et amélioration de la documentation

--- src/main/java/com/safetynet/alerts/controller/FireStationController.java
+++ src/main/java/com/safetynet/alerts/controller/FireStationController.java
@@ -6,6 +6,7 @@ import com.safetynet.alerts.dto.StationCoverageDTO;
 import com.safetynet.alerts.model.FireStation;
 import com.safetynet.alerts.service.FireStationService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -15,6 +16,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.tinylog.Logger;
 
 import java.util.List;
 import java.util.Set;
@@ -36,11 +38,14 @@ public class FireStationController {
     @Operation(summary = "Creates a fire station", description = "Creates a fire station with his number and the address.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Fire station has been created"),
+            @ApiResponse(responseCode = "400", description = "Fire station with specified data are not valid.",
+                    content = {@Content(schema = @Schema(implementation = Error.class))}),
             @ApiResponse(responseCode = "409", description = "Fire station with specified number and address already exists.",
                     content = {@Content(schema = @Schema(implementation = Error.class))})
     })
     @PostMapping("/firestation")
     public ResponseEntity<Void> create(@RequestBody @Valid FireStation fireStation) {
+        Logger.info("Request to create a new fire station : {}", fireStation);
         return fireStationService.create(fireStation);
     }
 
@@ -48,11 +53,14 @@ public class FireStationController {
     @Operation(summary = "Updates a fire station", description = "Updates a fire station by the address")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Fire station has been updated"),
+            @ApiResponse(responseCode = "400", description = "Fire station with specified data are not valid.",
+                    content = {@Content(schema = @Schema(implementation = Error.class))}),
             @ApiResponse(responseCode = "404", description = "Fire station with specified lastName and firstName was not found.",
                     content = {@Content(schema = @Schema(implementation = Error.class))})
     })
     @PutMapping("/firestation")
     public ResponseEntity<Void> update(@RequestBody @Valid FireStation fireStation) {
+        Logger.info("Request to update a fire station : {}", fireStation);
         return fireStationService.update(fireStation);
     }
 
@@ -65,6 +73,7 @@ public class FireStationController {
     })
     @DeleteMapping("/firestation")
     public ResponseEntity<Void> delete(@RequestBody @Valid FireStation fireStation) {
+        Logger.info("Request to delete a fire station : {}", fireStation);
         return fireStationService.delete(fireStation);
     }
 
@@ -77,12 +86,15 @@ public class FireStationController {
                     content = {@Content(schema = @Schema(implementation = Error.class))})
     })
     @GetMapping("/firestation")
-    public StationCoverageDTO getPersonsStationCoverage(@RequestParam("stationnumber") int stationNumber) {
+    public StationCoverageDTO getPersonsStationCoverage(
+            @RequestParam("stationnumber")
+            @Parameter(description = "The station number where we want to obtain the people covered by this one", example = "1")
+            int stationNumber) {
         return fireStationService.getPersonsStationCoverage(stationNumber);
     }
 
 
-    @Operation(summary = "Get all homes for each stations", description = "Get all homes for each stations order by station number and group by address. The information of each resident contains the lastName, the phone number, the age and the medical record.")
+    @Operation(summary = "Get all homes for each stations", description = "Get all homes for each stations order by station number and group by address. The information of each resident contains the last name, first name, the phone number, the age and the medical record.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "List of information for specified stations numbers.",
                     content = {@Content(schema = @Schema(implementation = FloodDTO.class))}),
@@ -90,7 +102,11 @@ public class FireStationController {
                     content = {@Content(schema = @Schema(implementation = Error.class))})
     })
     @GetMapping("/flood/stations")
-    public List<FloodDTO> getHomesByStations(@RequestParam("stations") List<Integer> stations) {
+    public List<FloodDTO> getHomesByStations(
+            @RequestParam("stations")
+            @Parameter(description = "The numbers of the stations for which we want to obtain residents' information", example = "[1, 2]")
+            List<Integer> stations) {
+        Logger.info("Request to get homes by this one or several stations : {}", stations);
         return fireStationService.getHomesByStations(stations);
     }
 
@@ -103,12 +119,16 @@ public class FireStationController {
                     content = {@Content(schema = @Schema(implementation = Error.class))})
     })
     @GetMapping("/phonealert")
-    public Set<String> getPersonsPhonesByStation(@RequestParam("firestation") int stationNumber) {
+    public Set<String> getPersonsPhonesByStation(
+            @RequestParam("firestation")
+            @Parameter(description = "The station number where we want to obtain all the telephone numbers of the people covered by it", example = "1")
+            int stationNumber) {
+        Logger.info("Request to get telephone numbers of persons covered by the station : {}", stationNumber);
         return fireStationService.getPersonsPhonesByStation(stationNumber);
     }
 
 
-    @Operation(summary = "Get all resident and the fire station by a specified address", description = "Get all resident and the fire station by a specified address. The list contains the persons information (lastName, firstName, phone, age, the medical record), and the fire station number")
+    @Operation(summary = "Get all resident and the fire station by a specified address", description = "Get all resident and the fire station by a specified address. The list contains the persons information (last name, first name, phone, age, the medical record), and the fire station number")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "List of resident and the fire station by a specified address.",
                     content = {@Content(schema = @Schema(implementation = FireDTO.class))}),
@@ -116,7 +136,11 @@ public class FireStationController {
                     content = {@Content(schema = @Schema(implementation = Error.class))})
     })
     @GetMapping("/fire")
-    public FireDTO getPersonsAndStationByAddress(@RequestParam("address") String address) {
+    public FireDTO getPersonsAndStationByAddress(
+            @RequestParam("address")
+            @Parameter(description = "The address from which you wish to obtain information on the inhabitants and the station number", example = "\"1509 Culver St\"")
+            String address) {
+        Logger.info("Request to get persons and the station by the address : {}", address);
         return fireStationService.getPersonsAndStationByAddress(address);
     }
 }
--- src/main/java/com/safetynet/alerts/controller/MedicalRecordController.java
+++ src/main/java/com/safetynet/alerts/controller/MedicalRecordController.java
@@ -13,6 +13,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.tinylog.Logger;
 
 
 @Tag(name = "Medical record API")
@@ -27,38 +28,48 @@ public class MedicalRecordController {
         this.medicalRecordService = medicalRecordService;
     }
 
-    @Operation(summary = "Creates a medical record", description = "Creates a medical record with a lastName, firstName, birthdate, and medications.")
+
+    @Operation(summary = "Creates a medical record", description = "Creates a medical record with a last name, first name, birthdate, and medications.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Medical record has been created"),
-            @ApiResponse(responseCode = "409", description = "Medical record with specified lastName and firstName already exists.",
+            @ApiResponse(responseCode = "400", description = "Medical record with specified data are not valid.",
+                    content = {@Content(schema = @Schema(implementation = Error.class))}),
+            @ApiResponse(responseCode = "409", description = "Medical record with specified last name and first name already exists.",
                     content = {@Content(schema = @Schema(implementation = Error.class))})
     })
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody @Valid MedicalRecord medicalRecord) {
+        Logger.info("Request to create a new medical record : {}", medicalRecord);
         return medicalRecordService.create(medicalRecord);
     }
 
 
-    @Operation(summary = "Updates a medical record", description = "Updates a medical record by the lastName and the firstName.")
+    @Operation(summary = "Updates a medical record", description = "Updates a medical record by the last name and the first name.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Medical record has been updated"),
-            @ApiResponse(responseCode = "404", description = "Medical record with specified lastName and firstName was not found.",
+            @ApiResponse(responseCode = "400", description = "Medical record with specified data are not valid.",
+                    content = {@Content(schema = @Schema(implementation = Error.class))}),
+            @ApiResponse(responseCode = "404", description = "Medical record with specified last name and first name was not found.",
                     content = {@Content(schema = @Schema(implementation = Error.class))})
     })
     @PutMapping
     public ResponseEntity<Void> update(@RequestBody @Valid MedicalRecord medicalRecord) {
+        Logger.info("Request to update a medical record : {}", medicalRecord);
         return medicalRecordService.update(medicalRecord);
     }
 
 
-    @Operation(summary = "Deletes a medical record", description = "Deletes a medical record by the lastName and the firstName.")
+    @Operation(summary = "Deletes a medical record", description = "Deletes a medical record by the last name and the first name.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Medical record has been deleted"),
-            @ApiResponse(responseCode = "404", description = "Medical record with specified lastName and firstName was not found.",
+            @ApiResponse(responseCode = "400", description = "First name and / or last name must be specified.",
+                    content = {@Content(schema = @Schema(implementation = Error.class))}),
+            @ApiResponse(responseCode = "404", description = "Medical record with specified last name and first name was not found.",
                     content = {@Content(schema = @Schema(implementation = Error.class))})
     })
     @DeleteMapping
     public ResponseEntity<Void> delete(@RequestBody @Valid PersonIdentifierDTO personIdentifier) {
+        Logger.info("Request to delete a medical record : {}", personIdentifier);
         return medicalRecordService.delete(personIdentifier);
     }
 }
