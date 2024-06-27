1291da9d71ac5f898b8f48fded725fa4e71060d4
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-06-27 21:52:45 +0200

    Ajout de logger et de la javadoc

--- src/main/java/com/safetynet/alerts/service/MedicalRecordService.java
+++ src/main/java/com/safetynet/alerts/service/MedicalRecordService.java
@@ -5,73 +5,120 @@ import com.safetynet.alerts.exception.AlreadyExistException;
 import com.safetynet.alerts.exception.NotFoundException;
 import com.safetynet.alerts.model.Data;
 import com.safetynet.alerts.model.MedicalRecord;
-import com.safetynet.alerts.repository.JsonFileReader;
+import com.safetynet.alerts.repository.JsonFileHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.tinylog.Logger;
 
 import java.util.List;
 import java.util.stream.IntStream;
 
+
+/**
+ * Service class for managing medical records.
+ * This class provides methods for creating, updating, and deleting medical records.
+ *
+ * @author Perrine Dassonville
+ * @version 1.0
+ * @see PersonIdentifierDTO
+ * @see AlreadyExistException
+ * @see NotFoundException
+ * @see Data
+ * @see MedicalRecord
+ * @see JsonFileHandler
+ */
 @Service
 public class MedicalRecordService {
 
-    private final JsonFileReader jsonFileReader;
+    private final JsonFileHandler jsonFileHandler;
 
     @Autowired
-    public MedicalRecordService(JsonFileReader jsonFileReader) {
-        this.jsonFileReader = jsonFileReader;
+    public MedicalRecordService(JsonFileHandler jsonFileHandler) {
+        this.jsonFileHandler = jsonFileHandler;
     }
 
 
+    /**
+     * Creates a new medical record.
+     *
+     * @param medicalRecord The medical record to create.
+     * @return A ResponseEntity with the HTTP status.
+     * @throws AlreadyExistException if a medical record with the same first and last name already exists.
+     */
     public ResponseEntity<Void> create(MedicalRecord medicalRecord) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Creating medical record : {}", medicalRecord);
+        Data data = jsonFileHandler.getData();
 
         boolean medicalRecordExists = data.medicalRecords().stream()
                 .anyMatch(p -> p.firstName().equals(medicalRecord.firstName()) && p.lastName().equals(medicalRecord.lastName()));
 
         if (medicalRecordExists) {
+            Logger.error(medicalRecord.firstName() + " " + medicalRecord.lastName() + " already exists");
             throw new AlreadyExistException(medicalRecord.firstName() + " " + medicalRecord.lastName() + " already exists");
         }
 
         data.medicalRecords().add(medicalRecord);
-        jsonFileReader.writeData(data);
+        jsonFileHandler.writeData(jsonFileHandler.sortMedicalRecordsByLastNameAndFirstName(data));
 
+        Logger.info("Medical record created : {}", medicalRecord);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 
+    /**
+     * Updates an existing medical record.
+     *
+     * @param medicalRecord The medical record to update.
+     * @return A ResponseEntity with the HTTP status.
+     * @throws NotFoundException if no medical record with the same first and last name is found.
+     */
     public ResponseEntity<Void> update(MedicalRecord medicalRecord) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Updating medical record : {}", medicalRecord);
+        Data data = jsonFileHandler.getData();
 
         List<MedicalRecord> medicalRecords = data.medicalRecords();
         int index = IntStream.range(0, medicalRecords.size())
                 .filter(i -> medicalRecords.get(i).firstName().equals(medicalRecord.firstName()) && medicalRecords.get(i).lastName().equals(medicalRecord.lastName()))
                 .findFirst()
-                .orElseThrow(() -> new NotFoundException("No medical record found for : " + medicalRecord.firstName() + " " + medicalRecord.lastName()));
+                .orElseThrow(() -> {
+                    Logger.error("No medical record found for : " + medicalRecord.firstName() + " " + medicalRecord.lastName());
+                    return new NotFoundException("No medical record found for : " + medicalRecord.firstName() + " " + medicalRecord.lastName());
+                });
 
-        if (!medicalRecords.get(index).equals(medicalRecord)) {
-            medicalRecords.set(index, medicalRecord);
-            jsonFileReader.writeData(data);
-        }
+        medicalRecords.set(index, medicalRecord);
+        jsonFileHandler.writeData(data);
 
+        Logger.info("Medical record updated : {}", medicalRecord);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
+    /**
+     * Deletes a medical record.
+     *
+     * @param personIdentifier The identifier of the person whose medical record to delete.
+     * @return A ResponseEntity with the HTTP status.
+     * @throws NotFoundException if no medical record with the same first and last name is found.
+     */
     public ResponseEntity<Void> delete(PersonIdentifierDTO personIdentifier) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Deleting medical record of : {}", personIdentifier);
+        Data data = jsonFileHandler.getData();
 
         MedicalRecord medicalRecordToDelete = data.medicalRecords().stream()
                 .filter(medicalRecord -> medicalRecord.firstName().equalsIgnoreCase(personIdentifier.firstName())
                         && medicalRecord.lastName().equalsIgnoreCase(personIdentifier.lastName()))
                 .findFirst()
-                .orElseThrow(() -> new NotFoundException(personIdentifier.firstName() + " " + personIdentifier.lastName() + " not found"));
+                .orElseThrow(() -> {
+                    Logger.error(personIdentifier.firstName() + " " + personIdentifier.lastName() + " not found");
+                    return new NotFoundException(personIdentifier.firstName() + " " + personIdentifier.lastName() + " not found");
+                });
 
         data.medicalRecords().remove(medicalRecordToDelete);
-        jsonFileReader.writeData(data);
+        jsonFileHandler.writeData(data);
 
+        Logger.info("Person deleted : {}", medicalRecordToDelete);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/safetynet/alerts/service/PersonService.java
+++ src/main/java/com/safetynet/alerts/service/PersonService.java
@@ -5,7 +5,7 @@ import com.safetynet.alerts.dto.PersonIdentifierDTO;
 import com.safetynet.alerts.dto.PersonInfoDTO;
 import com.safetynet.alerts.exception.AlreadyExistException;
 import com.safetynet.alerts.model.MedicalRecord;
-import com.safetynet.alerts.repository.JsonFileReader;
+import com.safetynet.alerts.repository.JsonFileHandler;
 import com.safetynet.alerts.exception.NotFoundException;
 import com.safetynet.alerts.model.Data;
 import com.safetynet.alerts.model.Person;
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.tinylog.Logger;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -23,73 +24,131 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static com.safetynet.alerts.util.BirthdateUtil.getAge;
-import static com.safetynet.alerts.util.BirthdateUtil.isChild;
+import static com.safetynet.alerts.util.BirthdateUtil.*;
 
+
+/**
+ * Service class for managing persons.
+ * This class provides methods for creating, updating, deleting, and retrieving persons.
+ * It also provides methods for getting persons by lastname, emails by city, and children by address.
+ *
+ * @author Perrine Dassonville
+ * @version 1.0
+ *
+ * @see ChildInfoDTO
+ * @see PersonIdentifierDTO
+ * @see PersonInfoDTO
+ * @see AlreadyExistException
+ * @see NotFoundException
+ * @see MedicalRecord
+ * @see Person
+ * @see JsonFileHandler
+ */
 @Service
 public class PersonService {
 
-    private final JsonFileReader jsonFileReader;
+    private final JsonFileHandler jsonFileHandler;
 
     @Autowired
-    public PersonService(JsonFileReader jsonFileReader) {
-        this.jsonFileReader = jsonFileReader;
+    public PersonService(JsonFileHandler jsonFileHandler) {
+        this.jsonFileHandler = jsonFileHandler;
     }
 
 
+    /**
+     * Creates a new person.
+     *
+     * @param person The person to create.
+     * @return A ResponseEntity with the HTTP status.
+     * @throws AlreadyExistException if a person with the same first and last name already exists.
+     */
     public ResponseEntity<Void> create(Person person) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Creating person : {}", person);
+        Data data = jsonFileHandler.getData();
 
         boolean personExists = data.persons().stream()
                 .anyMatch(p -> p.firstName().equals(person.firstName()) && p.lastName().equals(person.lastName()));
 
         if (personExists) {
+            Logger.error(person.firstName() + " " + person.lastName() + " already exists");
             throw new AlreadyExistException(person.firstName() + " " + person.lastName() + " already exists");
         }
 
         data.persons().add(person);
-        jsonFileReader.writeData(data);
+        jsonFileHandler.writeData(jsonFileHandler.sortPersonsByLastNameAndFirstName(data));
 
+        Logger.info("Person created : {}", person);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 
+    /**
+     * Updates an existing person.
+     *
+     * @param person The person to update.
+     * @return A ResponseEntity with the HTTP status.
+     * @throws NotFoundException if no person with the same first and last name is found.
+     */
     public ResponseEntity<Void> update(Person person) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Updating person : {}", person);
+        Data data = jsonFileHandler.getData();
 
         List<Person> persons = data.persons();
         int index = IntStream.range(0, persons.size())
                 .filter(i -> persons.get(i).firstName().equals(person.firstName()) && persons.get(i).lastName().equals(person.lastName()))
                 .findFirst()
-                .orElseThrow(() -> new NotFoundException(person.firstName() + " " + person.lastName() + " not found"));
+                .orElseThrow(() -> {
+                    Logger.error(person.firstName() + " " + person.lastName() + " not found");
+                    return new NotFoundException(person.firstName() + " " + person.lastName() + " not found");
+                });
 
-        if (!persons.get(index).equals(person)) {
-            persons.set(index, person);
-            jsonFileReader.writeData(data);
-        }
+        persons.set(index, person);
+        jsonFileHandler.writeData(data);
 
+        Logger.info("Person updated : {}", person);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
+    /**
+     * Deletes a person.
+     *
+     * @param personIdentifier The identifier of the person to delete.
+     * @return A ResponseEntity with the HTTP status.
+     * @throws NotFoundException if no person with the same first and last name is found.
+     */
     public ResponseEntity<Void> delete(PersonIdentifierDTO personIdentifier) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Deleting person : {}", personIdentifier);
+        Data data = jsonFileHandler.getData();
 
         Person personToDelete = data.persons().stream()
                 .filter(person -> person.firstName().equalsIgnoreCase(personIdentifier.firstName())
                         && person.lastName().equalsIgnoreCase(personIdentifier.lastName()))
                 .findFirst()
-                .orElseThrow(() -> new NotFoundException(personIdentifier.firstName() + " " + personIdentifier.lastName() + " not found"));
+                .orElseThrow(() -> {
+                    Logger.error(personIdentifier.firstName() + " " + personIdentifier.lastName() + " not found");
+                    return new NotFoundException(personIdentifier.firstName() + " " + personIdentifier.lastName() + " not found");
+                });
 
         data.persons().remove(personToDelete);
-        jsonFileReader.writeData(data);
+        jsonFileHandler.writeData(data);
 
+        Logger.info("Person deleted : {}", personToDelete);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
+    /**
+     * Retrieves persons by their last name.
+     *
+     * @param lastname The last name to search for.
+     * @return A list of PersonInfoDTOs of persons with the given last name.
+     * @throws NotFoundException if no persons with the given last name are found.
+     */
     public List<PersonInfoDTO> getPersonByLastname(String lastname) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Getting persons by lastname : {}", lastname);
+
+        Data data = jsonFileHandler.getData();
         Map<Person, MedicalRecord> personMedicalRecordMap = mapPersonsToMedicalRecords(data);
 
         List<PersonInfoDTO> personsInfo = data.persons().stream()
@@ -106,15 +165,25 @@ public class PersonService {
                 .toList();
 
         if (personsInfo.isEmpty()) {
+            Logger.error("Lastname: " + lastname + " not found");
             throw new NotFoundException("Lastname: " + lastname + " not found");
         }
 
+        Logger.info("Successfully got persons by lastname : {}", lastname);
         return personsInfo;
     }
 
 
+    /**
+     * Retrieves emails of persons living in a given city.
+     *
+     * @param city The city to search for.
+     * @return A set of emails of persons living in the given city.
+     * @throws NotFoundException if no persons living in the given city are found.
+     */
     public Set<String> getEmailsByCity(String city) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Getting emails by city : {}", city);
+        Data data = jsonFileHandler.getData();
 
         Set<String> emails = data.persons().stream()
                 .filter(person -> person.city().equalsIgnoreCase(city))
@@ -122,18 +191,28 @@ public class PersonService {
                 .collect(Collectors.toSet());
 
         if (emails.isEmpty()) {
+            Logger.error("City: " + city + " not found");
             throw new NotFoundException("City: " + city + " not found");
         }
 
+        Logger.info("Successfully got emails by city : {}", city);
         return emails;
     }
 
 
+    /**
+     * Retrieves children living at a given address.
+     *
+     * @param address The address to search for.
+     * @return A list of ChildInfoDTOs of children living at the given address.
+     */
     public List<ChildInfoDTO> getChildrenByAddress(String address) {
-        Data data = jsonFileReader.getData();
+        Logger.info("Getting children by address : {}", address);
+
+        Data data = jsonFileHandler.getData();
         Map<Person, MedicalRecord> personMedicalRecordMap = mapPersonsToMedicalRecords(data);
 
-        List<ChildInfoDTO> childrenInfo = data.persons().stream()
+        List<ChildInfoDTO> children = data.persons().stream()
                 .filter(person -> person.address().equalsIgnoreCase(address))
                 .map(person -> {
                     MedicalRecord medicalRecord = personMedicalRecordMap.get(person);
@@ -152,10 +231,17 @@ public class PersonService {
                 .filter(Objects::nonNull)
                 .toList();
 
-        return childrenInfo;
+        Logger.info("Successfully got children by address : {}", address);
+        return children;
     }
 
 
+    /**
+     * Maps persons to their medical records.
+     *
+     * @param data The data containing the persons and medical records.
+     * @return A map of persons to their medical records.
+     */
     public static Map<Person, MedicalRecord> mapPersonsToMedicalRecords(Data data) {
         return data.persons().stream()
                 .collect(Collectors.toMap(
